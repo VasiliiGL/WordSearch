78fc85f483d5e448fc7e1636afc3d937f8a744cb
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-07-30 10:30:49 +0400

    инизиализация класса модели, определение данных для модели. Черновик

---
+++ Word Search/ModelData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Word_Search
+{
+    class ModelData : INotifyPropertyChanged
+    {
+        private string searchWords;
+        private string pathFile;
+        private string newPathFile;
+
+        public string SearchWords
+        {
+            get { return searchWords; }
+            set
+            {
+                searchWords = value;
+                OnPropertyChanged("SearchWords");
+            }
+        }
+        public string PathFile
+        {
+            get { return pathFile; }
+            set
+            {
+                pathFile = value;
+                OnPropertyChanged("PathFile");
+            }
+        }
+        public string NewPathFile
+        {
+            get { return newPathFile; }
+            set
+            {
+                newPathFile = value;
+                OnPropertyChanged("NewPathFile");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
+
