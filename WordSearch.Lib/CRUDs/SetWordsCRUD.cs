c1c331877e043f90a9a994cb9337463fda59bf05
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-08-20 11:47:17 +0400

    Добавление классов управления моделями File, Text, SetWords (список слов)

---
+++ WordSearch.Lib/CRUDs/SetWordsCRUD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch.Models.CRUDs
+{
+    public class SetWordsCRUD
+    {
+        public void AddFromString(DataViewModels data)
+        {
+            string words = data.SelectedWord.WordSearch;
+            string[] newSetWords = words.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in newSetWords)
+            {
+                var newWord = new Word { WordSearch = word };
+                data.SetWords.Add(newWord);
+            }
+
+        }
+    }
+}
---
+++ WordSearch.Lib/CRUDs/TextCRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WordSearch.Models.CRUDs
+{
+    public class TextCRUD
+    {
+        public string DeleteSigns(string text)
+        {
+            Regex regex = new Regex(@"\W");
+            string newText = regex.Replace(text, " ");
+            return newText;
+        }
+    }
+}
