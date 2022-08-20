166ccd9aa7475b65af551dbfa79a86968d6bec72
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-08-20 08:59:01 +0400

    new solution for the project

--- Word Search/MainWindow.xaml.cs
+++ Word Search/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,9 @@ namespace Word_Search
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new ApplicationViewModel();
+            
         }
+
+       
     }
 }
