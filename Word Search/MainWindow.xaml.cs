3ea9feeeb4b9c0496b04c87c5f1d599cefac10b7
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-08-20 11:51:54 +0400

    Добвление действия для кнопки загрузки слов, привязка данных

--- Word Search/MainWindow.xaml.cs
+++ Word Search/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WordSearch.Models;
 
 namespace Word_Search
 {
@@ -21,12 +22,35 @@ namespace Word_Search
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataViewModels data = new DataViewModels();
         public MainWindow()
         {
             InitializeComponent();
-            
+            DataContext = data;
         }
 
-       
+        private void SelectWords_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dialog = new OpenFileDialog();
+                dialog.FileName = "Запрещенные слова 1";
+                dialog.DefaultExt = ".txt";
+                dialog.Filter = "Text documents (.txt)|*.txt";
+                dialog.InitialDirectory = @"D:\VASILII\Запрещенные слова";
+                if (dialog.ShowDialog() == true)
+                {
+                    string filename = dialog.FileName;
+                    var word = new Word { WordSearch = data.TextCrud.DeleteSigns(System.IO.File.ReadAllText(filename)) };
+                    data.SelectedWord = word;
+                }
+                data.SetWordsCRUD.AddFromString(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+        }
     }
 }
