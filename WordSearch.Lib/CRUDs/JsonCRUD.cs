dec954a026aa5d06591523c446170360b7ff7f6b
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-08-27 10:42:22 +0400

    Класс для управления Json файлом для хранения начальных данных для работы программы

---
+++ WordSearch.Lib/CRUDs/JsonCRUD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WordSearch.Models.Lib;
+
+namespace WordSearch.Models.CRUDs
+{
+    public class JsonCRUD
+    {
+        //public Initial initialData { get; set; }
+        public async Task SaveJsonAsync(Initial initialData)
+        {
+            FileStream fs = new FileStream("InitilData.json", FileMode.OpenOrCreate);          
+            await JsonSerializer.SerializeAsync<Initial>(fs, initialData);
+        }
+        public void SaveJson(string initialDirectoryForWords)
+        {
+            FileStream fs = new FileStream("InitilData.json", FileMode.OpenOrCreate);
+            Initial initialData = new Initial(initialDirectoryForWords);
+        }
+    }
+}
