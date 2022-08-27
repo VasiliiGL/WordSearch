c071d358b7b2a6eef5ecdba4aa5673b64d41a773
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-08-27 15:35:32 +0400

    Добавлена заготовка для построения графика. Получение актуальных данных не реализовано

--- Word Search/ReportWindow.xaml.cs
+++ Word Search/ReportWindow.xaml.cs
@@ -22,6 +22,19 @@ namespace Word_Search
         {
             InitializeComponent();
             DataContext = data;
+        
+            //double[] values = { 26, 20, 23, 7, 16 };
+            double[] values = GetValues(data);
+            //double[] positions = { 0, 1, 2, 3, 4 };
+            double[] positions = GetPositions(data);
+            //string[] labels = { "PHP", "JS", "C++", "GO", "VB" };
+            string[] labels = GetWordsForLabels(data);
+            ChartRatingWords.Plot.AddBar(values, positions);
+            ChartRatingWords.Plot.XTicks(positions, labels);
+            ChartRatingWords.Plot.SetAxisLimits(yMin: 0);
+
+            ChartRatingWords.Plot.SaveFig("bar_labels.png");
+            ChartRatingWords.Refresh();
         }
 
         private void ListFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -29,5 +42,37 @@ namespace Word_Search
             var text = ((FileSearch)ListFiles.SelectedItem).Text;
             MessageBox.Show(text);
         }
+
+        private string[] GetWordsForLabels(DataViewModels data)
+        {
+            var size = data.ListWords.Count;
+            string[] labels = new string[size];
+            for (var i=0; i<size; i++)
+            {
+                labels[i] = data.ListWords[i].WordSearch;
+            }
+            return labels;
+        }
+        private double[] GetValues(DataViewModels data)
+        {
+            var size = data.ListWords.Count;
+            double[] values = new double[size];
+            for (var i = 1; i <= size; i++)
+            {
+                values[i-1] = i*10;
+            }
+            return values;
+        }
+        private double[] GetPositions(DataViewModels data)
+        {
+            var size = data.ListWords.Count;
+            double[] positions = new double[size];
+            for (var i = 0; i < size; i++)
+            {
+                positions[i] = i;
+            }
+            return positions;
+        }
+
     }
 }
