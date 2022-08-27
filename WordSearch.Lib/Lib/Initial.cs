4da923fbf58d0dd3fb5d829b22a535a4808f8c8f
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-08-27 10:40:49 +0400

    Добавлен класс Initial для хранения начальных данных для работы : стартовая директория, путь для директории хранения копии файлов

---
+++ WordSearch.Lib/Lib/Initial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch.Models.Lib
+{
+    public class Initial
+    {
+        public string DirectoryForWords { get; set; }
+        public string DirectoryForCopyFile { get; set; }
+        public Initial()
+        {
+            DirectoryForWords = @"D:\VASILII\Запрещенные слова";
+            DirectoryForCopyFile = @"D:\VASILII\Контрольная работа WordSearch\NewDirectory";
+        }
+        public Initial(string directoryForWords)
+        {
+            DirectoryForWords = directoryForWords;          
+        }
+    }
+}
