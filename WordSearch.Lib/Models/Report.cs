4aa0ec230574542c6acc896f63c3a519bcd6a27d
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-09-16 11:17:17 +0400

    Создание класса для сохранения файла отчета

---
+++ WordSearch.Lib/Models/Report.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using WordSearch.Models.CRUDs;
+
+namespace WordSearch.Models.Models
+{
+    public class Report: DataViewModels
+    {
+        string pathReportFile = @"D:\VASILII\Контрольная работа WordSearch\NewDirectory\REPORT.txt";
+        string listWord;
+
+        public Report(DataViewModels data)
+        {
+            Directory = data.Directory;
+            ListDangerFiles = data.ListDangerFiles;
+            ListWords = data.ListWords;
+            SaveFileReport();
+        }
+        public string GetPath()
+        {
+            return pathReportFile;
+        }
+        public string GetListWordToString()
+        {
+            foreach (var word in ListWords)
+            {
+                listWord = listWord + " " + string.Join(" ", word.WordSearch);
+            }
+            return listWord;
+        }
+
+
+        public void SaveFileReport()
+        {
+            FileCRUD.CrearFileReport(pathReportFile);
+            FileCRUD.SaveTextInFile($"Cлова для поиска: {GetListWordToString()}",pathReportFile);
+            foreach (var file in ListDangerFiles )
+            {
+                SaveFileReport(file);
+            }
+            
+        }
+        public void SaveFileReport(FileSearch file)
+        {
+            string nameFile = file.NameFile;
+
+            FileCRUD.SaveTextInFile($"Имя файла: {nameFile}", pathReportFile);
+            FileCRUD.SaveTextInFile($"Опасные слова: {file.ListWordsReport}", pathReportFile);
+
+        }
+    }
+}
